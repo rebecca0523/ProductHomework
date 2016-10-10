cff5423996b7250a6e63e44e487c4f49b10aeb84
yachin <selina66@ms68.hinet.net>
2016-10-10 22:50:18 +0800

    try commit again

--- ProductHomework/Class1.cs
+++ ProductHomework/Class1.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace ProductHomework
 { /// <summary>
-/// 1.mark first
-/// 
+/// 1.summary mark first
+/// 2.create git success
+/// 3.update Git Success
+/// 3.1 file commit 
+/// 3.1 Team Explorer
+/// 3.2 Sync
+/// 3.3 Push 
 /// </summary>
     public class Product1
     {
